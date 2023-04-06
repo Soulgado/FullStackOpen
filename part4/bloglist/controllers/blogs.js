1da761804aa4b6e43f84d770736e5b5c91191f76
Soulgado <nleonovq@gmail.com>
2023-04-06 21:43:47 +0200

    Exercises 4.13-4.14

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,12 +1,12 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get('/', async (request, response) => {
+blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs);
 });
   
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post("/", async (request, response, next) => {
   const body = request.body;
 
   if (!body.title || !body.url) {
@@ -23,6 +23,29 @@ blogsRouter.post('/', async (request, response) => {
     response.status(201).json(newBlog);
   } catch (error) {
     console.log("controller error: ", error);
+    next(error);
+  }
+});
+
+blogsRouter.delete("/:id", async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id);
+    response.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
+
+blogsRouter.put("/:id", async (request, response, next) => {
+  const { title, author, likes } = request.body;
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      request.params.id,
+      { title, author, likes },
+      { new: true }
+    );
+    response.json(updatedBlog);
+  } catch (error) {
     next(error);
   }
 });
--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -14,88 +14,146 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-test("blogs are returned in json format", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-}, 100000);
-
-test("request to api/blogs returns all blogs", async () => {
-  const response = await api.get("/api/blogs");
-
-  expect(response.body).toHaveLength(testHelper.initialTestBlogs.length);
-});
-
-test("blogs have id property", async () => {
-  const response = await api.get("/api/blogs");
-
-  response.body.forEach(blog => {
-    expect(blog.id).toBeDefined();
+describe("when there is initially some blogs", () => {
+  test("blogs are returned in json format", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  }, 100000);
+  
+  test("request to api/blogs returns all blogs", async () => {
+    const response = await api.get("/api/blogs");
+  
+    expect(response.body).toHaveLength(testHelper.initialTestBlogs.length);
+  });
+  
+  test("blogs have id property", async () => {
+    const response = await api.get("/api/blogs");
+  
+    response.body.forEach(blog => {
+      expect(blog.id).toBeDefined();
+    });
   });
 });
 
-test("a blog can be added to the database", async () => {
-  const newBlog = {
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
-    likes: 10,
-  };
-
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-
-  const newResponse = await Blog.find({});
-  const newBlogList = newResponse.map(response => response.toJSON());
-
-  expect(newBlogList).toHaveLength(testHelper.initialTestBlogs.length + 1);
+describe("adding blogs to the database", () => {
+  test("a blog can be added to the database", async () => {
+    const newBlog = {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
+      likes: 10,
+    };
   
-  const blogsTitles = newBlogList.map(blog => blog.title);
-  expect(blogsTitles).toContain("First class tests");
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+  
+    const newResponse = await Blog.find({});
+    const newBlogList = newResponse.map(response => response.toJSON());
+  
+    expect(newBlogList).toHaveLength(testHelper.initialTestBlogs.length + 1);
+    
+    const blogsTitles = newBlogList.map(blog => blog.title);
+    expect(blogsTitles).toContain("First class tests");
+  });
+  
+  test("if likes property is missing from POST request, it defaults to value 0", async () => {
+    const newBlog = {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
+    };
+  
+    await api.post("/api/blogs").send(newBlog);
+  
+    const response = await Blog.find({ title: "First class tests" });
+    const blog = response[0];
+  
+    expect(blog.likes).toBe(0);
+  });
+  
+  test("if url or title are missing from POST request, server returns 400 Bad Request", async () => {
+    const newBlogWithoutTitle = {
+      _id: "5a422b891b54a676234d17fa",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
+      likes: 10,
+    };
+  
+    const newBlogWithoutUrl = {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      likes: 10,
+    };
+  
+    await api
+      .post("/api/blogs")
+      .send(newBlogWithoutTitle)
+      .expect(400);
+  
+    await api
+      .post("/api/blogs")
+      .send(newBlogWithoutUrl)
+      .expect(400);
+  }, 100000);
 });
 
-test("if likes property is missing from POST request, it defaults to value 0", async () => {
-  const newBlog = {
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
-  };
+describe("deleting blogs", () => {
+  test("request correctly deletes blog from the database", async () => {
+    const newBlog = {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
+      likes: 10,
+    };
 
-  await api.post("/api/blogs").send(newBlog);
+    await api.post("/api/blogs").send(newBlog);
 
-  const response = await Blog.find({ title: "First class tests" });
-  const blog = response[0].toJSON();
+    await api
+      .delete(`api/blogs/${newBlog._id}`)
+      .expect(204);
+    
+    const allBlogs = await Blog.find({});
+    expect(allBlogs).toHaveLength(testHelper.initialTestBlogs.length);
 
-  expect(blog.likes).toBe(0);
+    expect(allBlogs).not.toContainEqual(newBlog);
+  });
 });
 
-test("if url or title are missing from POST request, server returns 400 Bad Request", async () => {
-  const newBlogWithoutTitle = {
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
-    likes: 10,
-  };
+describe("updating individual blog posts", () => {
+  test("correctly updating number of likes", async () => {
+    const newBlog = {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
+      likes: 10,
+    };
 
-  const newBlogWithoutUrl = {
-    title: "First class tests",
-    author: "Robert C. Martin",
-    likes: 10,
-  };
+    await newBlog.save();
 
-  await api
-    .post("/api/blogs")
-    .send(newBlogWithoutTitle)
-    .expect(400);
+    await api
+      .put(`api/blogs/${newBlog._id}`)
+      .send({ ...newBlog, likes: 5 })
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
 
-  await api
-    .post("/api/blogs")
-    .send(newBlogWithoutUrl)
-    .expect(400);
-}, 100000);
+    const response = await Blog.findById(newBlog._id);
+    const changedBlog = response.body;
+
+    expect(changedBlog.likes).toBe(5);
+  });
+});
+
+
 
 afterAll(async () => {
   await mongoose.connection.close();
