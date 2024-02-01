b245c24e2df8ca99f30e555541b34f01c8f67cc5
Soulgado <nleonovq@gmail.com>
2024-02-01 21:13:54 +0200

    Exercise 8.4

--- part8/library-backend/index.js.js
+++ part8/library-backend/index.js.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     authorCount: Int!
     bookCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -125,7 +125,11 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) return books;
+
+      return books.filter(b => b.author === args.author);
+    },
     allAuthors: () => authors,
   },
   Author: {
